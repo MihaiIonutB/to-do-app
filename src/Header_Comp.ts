8f46f82bdffba9e505cc6900b1aed67507eb5544
Thedrunkhorse <ionutburghel@yahoo.com>
2019-11-29 14:04:06 +0200

    tsx conversion

---
+++ src/Header_Comp.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Display(){
+    return (
+        <div>
+            <h1 class = "header" , fontSize = '10px'> "Welcome" </h1>
+        </div>
+    )
+}
+
+export default Display;
