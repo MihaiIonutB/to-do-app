42c6ac64f135d8208c15dab8df03172c2601f7ec
Thedrunkhorse <ionutburghel@yahoo.com>
2019-11-29 14:32:00 +0200

    Input Commit

---
+++ src/Header_Comp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Display(){
+    return (
+        <div>
+            <h1 className= 'header' style = {{fontSize: '1.5em', textAlign:'center'}}> Welcome </h1>
+        </div>
+    )
+}
+
+export default Display;
---
+++ src/InputForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function User_Input(){
+    return (
+        <div>
+            <form classname = "userInput" type = "submit">
+                
+            </form>
+        </div>
+    )
+}
+export default User_Input;
